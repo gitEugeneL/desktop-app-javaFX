a7ccc215c61da40c43c5d9feaf544e4ca77ce5d0
Eug Lihon <44579271+euglihon@users.noreply.github.com>
2022-01-09 20:01:09 +0100

    rename view file

--- src/main/java/zaliczenie/javafx/desktopapp/App.java
+++ src/main/java/zaliczenie/javafx/desktopapp/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("student-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),760, 640);
         String cssFile = App.class.getResource("css/style.css").toExternalForm();
         scene.getStylesheets().add(cssFile);
